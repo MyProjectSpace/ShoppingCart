2a901a8e00e27dd62e65dda5f93e54bf71e03844
DESKTOP-JULFTCG\Miluka <milukadesilva88@gamil.com>
2026-04-04 15:37:23 +0800

    Reach shopping cart

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(StoreContext store) : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await store.Products.ToListAsync();
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await store.Products.FindAsync(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+    }
+}
